1f2ee545d434c65c0e534b4dbaa173f750e2cc68
Visnu <vgcaisse@gmail.com>
2022-04-19 22:51:48 -0400

    created my cars seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,35 @@
 // STRETCH
+const cars = [
+    {
+        vin: '3315844833158448',
+        make: 'F.A.',
+        model: 'Dawn Dragster',
+        mileage: 2100,
+        title: 'Light',
+        transmission: 'Boosted'
+    },
+    {
+        vin: '0676470606764706',
+        make: 'F.A.',
+        model: 'Dark Dragster',
+        mileage: 2100,
+        title: 'Dark',
+        transmission: 'Hyproded'
+    },
+    {
+        vin: '9344945093449450',
+        make: 'F.A.',
+        model: 'Hang On Mach',
+        mileage: 0,
+        title: 'Wind',
+        transmission: 'Darklawed'
+    },
+]
+
+exports.seed = function (knex) {
+    return knex('cars')
+        .truncate()
+        .then(() => {
+            return knex('cars').insert(cars)
+        })
+}
